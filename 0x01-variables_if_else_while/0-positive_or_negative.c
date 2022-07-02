885990ec71f798bc976c89c069a007de8aaf3798
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-02 12:26:00 -0700

    C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main -> assign a random number to the variable n each time it is executed and prints out 
+ * based a condition
+ * Return : always 0
+ */
 int main (void)
 {
 int x;
