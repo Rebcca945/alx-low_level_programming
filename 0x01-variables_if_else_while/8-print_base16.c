d4e84c7b57fe59a69e4bb543e1d3c2ee0e24d107
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-03 14:42:17 -0700

    C

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,13 +7,13 @@
  * and print the last digit of the number stored in the variable n
  * Return : always 0
  */
-void main (void)
+void main(void)
 {
 char ch;
 int n;
 for (n = 48; n <= 57; n++)
 {
- 	putchar(n);
+ putchar(n);
 }
 for (ch = 'a'; ch <= 'f'; ch++)
 {
