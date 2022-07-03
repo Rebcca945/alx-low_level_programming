30ea8bb52a4fc3129fbf9ea2c494414465bfdf31
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-03 14:24:00 -0700

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return : always 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch ='z'; ch >= 'a'; ch--)
+	{
+
+		putchar(ch);
+	}
+	putchar(10); /* this is an ascil code for new line*/
+
+	retutn (0);
+}
