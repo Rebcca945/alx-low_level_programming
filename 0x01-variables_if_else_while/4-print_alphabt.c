f984d26639101295413c7beef765cb3e9bb58306
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-03 07:16:47 -0700

    C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return : always 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= '2'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+
+		putchar(20);
+
+		return (0);
+}
