751062c9734e27a69e9b409fdf122a40649f4a84
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-04 08:42:35 -0700

    C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,20 +3,18 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
- * Return : always 0
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 int main(void)
 {
 int ch;
+
 for (ch = 48; ch <= 57; ch++)
 {
-		
 putchar(ch);
 }
-
 putchar(10);
-
 return (0);
 }
