ccf52c528fe968eb0e1c463b65e7fbb6d42cd556
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-04 08:28:43 -0700

    C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 
 /**
- * main -> assign a random number to the variable n each time it is executed 
- * and print the last digit of the number stored in the variable n
- * Return : always 0
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	int n;
+int n;
 
-	strand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is ", n);
-	if (n > 5)
-	{
-		printf("greater than 5");
-	}
-	if (n == 0)
-	{
-		printf("0");
-	}
-	if (n < 0 && n != 0)
-	{
-		printf("less than 0 not 0");
-	}
-	printf("\n");
+strand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	return (0);
+printf("Last digit of %d is ", n);
+if (n > 5)
+{
+printf("greater than 5");
+}
+if (n == 0)
+{
+printf("0");
+}
+if (n < 0 && n != 0)
+{
+printf("less than 0 not 0");
+}
+printf("\n");
+return (0);
 }
