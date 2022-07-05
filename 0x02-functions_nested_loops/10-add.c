b97daaf77e145429e85592cef598ec3038b2cc68
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-05 14:39:56 -0700

    C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds x and y
+ * @x: the passed args
+ * @y: the passed args
+ * Return: n
+ */
+
+int add(int x, int y)
+{
+int n;
+
+n = add(89, 9);
+printf("%d\n", n);
+return (0);
+}
