ea4d5e8563464f1e773f9d9b00e591d71a64333f
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-05 13:19:22 -0700

    C

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** print_last_digit - print last digit
+ * @n: the passed args
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+int x;
+
+if (n < 0)
+{
+n = -n;
+}
+x = n % 10;
+_putchar(x + '0');
+return (n % 10);
+}
