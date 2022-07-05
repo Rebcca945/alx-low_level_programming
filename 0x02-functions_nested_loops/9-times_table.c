7ee8e7aafaa8c157a64c2e63d3758ba965574cd4
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-05 14:29:57 -0700

    C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
+/**
+ * times_table - 9 times a number
+ * @n: the passed args
+ * Return: x
+ */
+void times_table(void);
+{
+int i;
+int x;
 
+for (i = 0; i < 10; i++)
+{
+x = 9 * i;
+_putchar(x);
+}
+}
