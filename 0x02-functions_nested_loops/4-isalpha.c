a75a3a0e4ca3cb9e28372414425fcbcd6fe3025d
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-05 12:46:17 -0700

    C

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isaplpha - checks for alphabetical letters
+ * @c: a charter to be check on
+ * Return: return 0 or 1
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
