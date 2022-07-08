3240b1cd459002ba6cea2ed0c32a4e56abf9f1a4
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-08 13:10:29 -0700

    C

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- *print_number - print with putchar
- *@n: takes number
- *Retutn: void
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 
 void print_number(int n)
