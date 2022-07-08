a44d89af59bc70f652b60ddc8a9ee089d3f94d74
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-08 13:06:36 -0700

    C

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_number - print with putchar
+ *@n: takes number
+ *Retutn: void
+ */
+
+void print_number(int n)
+{
+unsigned int m;
+
+if (n < o)
+{
+_putchar('-');
+m = -n;
+}
+else
+{
+m = n;
+}
+if (m / 10 != 0)
+print_number(m / 10);
+_putchar((m % 10) + '0');
+}
