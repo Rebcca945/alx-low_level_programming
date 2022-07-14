4465fcb046a00853ed2bb085a02a8b644f76105d
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-14 15:10:42 -0700

    c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+unsigned int num = n;
+
+if (n < 0)
+{
+_putchr('_');
+num = -num;
+}
+if ((num / 10 > 0)
+{
+_print_number(num / 10);
+}
+_putchar((num % 10) + '0');
+}
