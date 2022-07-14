90eaaa4e7419461e39e7a4f4bdbb8b7738dc57d6
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-14 14:58:46 -0700

    C

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string
+ * @str: target string
+ * Return: return string
+ */
+
+char *rot13(char *str)
+{
+int num1, num2;
+
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+	'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+	'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+	'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
+	'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+	'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c',
+	'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	'm'};
+
+while (str[++num1])
+{
+for (num2 = 0; num2 < 52; num2++)
+{
+if (str[num1] == alphabet[num2])
+{
+str[num1] = rot13key[num2];
+break;
+}
+}
+}
+return (str);
+}
