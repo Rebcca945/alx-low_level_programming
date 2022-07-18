a61f814269930db60c5347a77016b8099b5f00e0
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-17 21:31:54 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - memory copy
+ * @dest: destination memory
+ * @src: source memory
+ * @n: number of bytes
+ * Return: string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+
+for (a = 0; a < n; a++)
+{
+dest [a] = src[a];
+return (dest);
+}
