c40981cd0e65fe3f1e2cf69ab7444465c9e7c215
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-22 08:51:03 -0700

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int result = 0, num, i, j, k;
 
 for (i = 1; i < argc; i++)
 {
-for (j = 0; arg[i][j] != '\0'; j++)
+for (j = 0; argv[i][j] != '\0'; j++)
 {
 if (argv[i][j] > '9' || argv[i][j] < '0')
 {
