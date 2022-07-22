0109fa34873d4fbb605d1e064e3d3f5cc9b79df2
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-21 22:13:58 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argc parameter
+ * @argv: array
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
