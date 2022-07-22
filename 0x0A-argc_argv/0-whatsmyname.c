6e8e2a307127c0c71176571b4e931fbc1e6091e7
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-21 22:05:21 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argc parameter
+ * @argv: array
+ * Return: 0 if success
+ */
+
+int main (int argc _attribute_((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
