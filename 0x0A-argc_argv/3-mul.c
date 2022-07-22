3feafe39f6fa969f3efedff1dfa5904c11920ae5
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-21 22:27:57 -0700

    argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <atoi>
+
+/**
+ * main - prints its name
+ * @argc: argc parameter
+ * @argv: array
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+int result, num1, num2;
+
+if
+{
+printf("%s\n", "error");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+printf("%d\n", result);
+return (0);
+}
