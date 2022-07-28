428590563946baaad970e5c164584478a5614e05
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-27 22:16:13 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatnates two strings
+ * @s1: string1 
+ * @s2: string2
+ * @n: max number of bytes
+ * Return: if the function fails - NULL or a pointer to
+ * the concatenated space in memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
