a9b8bce482aa35918caca23d6ca492556b5946b3
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-28 14:55:50 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,14 +7,12 @@
  *
  * Return: A pointer to the allocated memory.
  */
-
 void *malloc_checked(unsigned int b)
 {
-void *s = malloc(b);
+void *p = malloc(b);
 
-if (s == NULL)
-}
-exit(98);
-}
-return (s);
+if (p == NULL)
+	exit(98);
+
+return (p);
 }
