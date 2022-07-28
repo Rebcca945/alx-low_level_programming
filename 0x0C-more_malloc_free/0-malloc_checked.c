db0468eac5133e793c09cfbc86279d93578d397f
Rebcca945 <rebccaassefa945@gmail.com>
2022-07-27 22:06:19 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - checks malloc
+ * @b: argument
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+if p = malloc(b);
+{
+exit(98);
+}
+return (p);
+}
