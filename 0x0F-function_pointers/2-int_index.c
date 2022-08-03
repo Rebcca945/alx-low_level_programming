95bf0e138f8e68a79bc6a50f64e1c4cb547dabb2
Rebcca945 <rebccaassefa945@gmail.com>
2022-08-03 09:21:38 -0700

    function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if (size <= 0 || cmp == NULL || array == NULL)
+if (size < 1 || cmp == NULL || array == NULL)
         return (-1);
 int i;
 
