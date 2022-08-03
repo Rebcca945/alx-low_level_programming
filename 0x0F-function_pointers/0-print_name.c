f88af17312ed0418a5c6eee5a8282c96a45f6ec6
Rebcca945 <rebccaassefa945@gmail.com>
2022-08-03 08:50:40 -0700

    function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,13 @@
-#ifndef name
-#define name
+#include "function_pointers.h"
 
 /**
- * print_name - prints a name.
- * @name: name to be printed
+ * print_name - prints a name
+ * @name: string
  * @f: function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 if (f != NULL)
-{
-f(name);
+	f(name);
 }
-}
-
-#endif
